b41e37fe2f4b4a7273f3d0bc956c3e8d899d9f74
ryanlabelle <ryan@honeycombweb.com>
2021-08-03 08:25:17 -0700

    Adding sub menus

--- hostel/assets/js/custom.js
+++ hostel/assets/js/custom.js
@@ -1,4 +1,31 @@
+// ======================= offcanvas menu ======================
+jQuery(document).ready(function($){
+  var $lateral_menu_trigger = $('#cd-menu-trsgger'),
+    $content_wrapper = $('.cd-main-content'),
+    $navigation = $('header');
 
+  //open-close lateral menu clicking on the menu icon
+  $lateral_menu_trigger.on('click', function(event){
+    event.preventDefault();
+    
+    $lateral_menu_trigger.toggleClass('is-clicked');
+    $navigation.toggleClass('lateral-menu-is-open');
+    $content_wrapper.toggleClass('lateral-menu-is-open').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+
+    // firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
+      $('body').toggleClass('overflow-hidden');
+    });
+    $('#cd-lateral-nav').toggleClass('lateral-menu-is-open');
+  });
+
+  //open (or close) submenu items in the lateral menu. Close all the other open submenu items.
+  $('.item-has-children').children('a').on('click', function(event){
+    event.preventDefault();
+    $(this).toggleClass('submenu-open').next('.sub-menu').slideToggle(200).end().parent('.item-has-children').siblings('.item-has-children').children('a').removeClass('submenu-open').next('.sub-menu').slideUp(200);
+  });
+  //
+  $('.thmv-leftside-menu li a.active').parent("item-has-children a").addClass('active');
+});
 
 // =============================sticky header Start Script====================
 document.addEventListener("DOMContentLoaded", function(){
