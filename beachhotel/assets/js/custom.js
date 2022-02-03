099f386e4b75304c37929e8e18fd8511cad3a7db
Dmitry Petrov <dmitrypetrov.dev@gmail.com>
2022-02-04 01:10:20 +0300

    .

--- beachhotel/assets/js/custom.js
+++ beachhotel/assets/js/custom.js
@@ -1,31 +1,103 @@
+// =============================sticky header Start Script====================
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 5) {
+            document.querySelector('.thmv-top-nav').classList.add('fixed-top');
+        } else {
+            document.querySelector('.thmv-top-nav').classList.remove('fixed-top');
+        }
+    });
+});
+// =============================sticky header End Script====================
+
+
+// ============================navigation menu============================= //
+(function ($) {
+    $.fn.menumaker = function (options) {
+        var cssmenu = $(this),
+            settings = $.extend({
+                format: "dropdown",
+                sticky: false
+            }, options);
+        return this.each(function () {
+            $(this).find(".button").on('click', function () {
+                $(this).toggleClass('menu-opened');
+                var mainmenu = $(this).next('ul');
+                if (mainmenu.hasClass('open')) {
+                    mainmenu.slideToggle().removeClass('open');
+                } else {
+                    mainmenu.slideToggle().addClass('open');
+                    if (settings.format === "dropdown") {
+                        mainmenu.find('ul').show();
+                    }
+                }
+            });
+
+            cssmenu.find('li ul').parent().addClass('has-sub');
+            multiTg = function () {
+                cssmenu.find(".has-sub").prepend('<span class="submenu-button"></span>');
+                cssmenu.find('.submenu-button').on('click', function () {
+                    $(this).toggleClass('submenu-opened');
+                    if ($(this).siblings('ul').hasClass('open')) {
+                        $(this).siblings('ul').removeClass('open').slideToggle();
+                    } else {
+                        $(this).siblings('ul').addClass('open').slideToggle();
+                    }
+                });
+            };
+
+            if (settings.format === 'multitoggle') multiTg();
+            else cssmenu.addClass('dropdown');
+            if (settings.sticky === true) cssmenu.css('position', 'fixed');
+            resizeFix = function () {
+                var mediasize = 991;
+                if ($(window).width() > mediasize) {
+                    cssmenu.find(' ul').show();
+                }
+                if ($(window).width() <= mediasize) {
+                    cssmenu.find(' ul').hide().removeClass('open');
+                }
+            };
+            resizeFix();
+            return $(window).on('resize', resizeFix);
+        });
+    };
+})(jQuery);
+
+(function ($) {
+    $(document).ready(function () {
+        $(".thmv-top-navbar").menumaker({
+            format: "multitoggle"
+        });
+    });
+})(jQuery);
+
+
 // rooms & suites slider //
-$(".slick-rooms-slider").slick({
-    infinite: true,
-    centerMode: true,
-    arrows: false,
-    pauseOnHover: true,
-    autoplay: true,
-    speed: 1000,
-    autoplaySpeed: 5000,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    responsive: [
-        {
-            breakpoint: 995,
-            settings: {
-                centerMode: false,
-                slidesToShow: 2,
-                slidesToScroll: 2
-            }
+(function () {
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        autoplay: {
+            delay: 4000,
         },
-        {
-            breakpoint: 768,
-            settings: {
-                centerMode: false,
-                slidesToShow: 1,
-                slidesToScroll: 1
+        loop: true,
+        speed: 1000,
+        slidesPerView: 4,
+        spaceBetween: 40,
+        autoHeight: true,
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 20
+            },
+            700: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            1200: {
+                slidesPerView: 4,
+                spaceBetween: 40
             }
         }
-    ]
-});
-
+    });
+})();
